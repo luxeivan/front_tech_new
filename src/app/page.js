1666fc822689ef8e02d1bf541ad9746240066a04
Stanislav <yanut995@gmail.com>
2025-03-25 15:46:54 +0300

    Сделал заглушку-пушку

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,35 @@
-"use server";
-import MainContent from "@/components/MainContent/MainContent";
-import { auth } from "@/config/auth";
+//Заглушечка, комментим её, когда не надо
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth();
-
-  const { token } = false;
-  return (
-    <div>
-      {session && (
-        <>
-          <h1 style={{ textAlign: "center" }}>
-            Добро пожаловать, {session.user?.name}
-          </h1>
-          <MainContent />
-        </>
-      )}
-      {!session && (
-        <h1 style={{ textAlign: "center", padding: 20 }}>
-          Пожалуйста авторизируйтесь
-        </h1>
-      )}
-    </div>
-  );
+  redirect("/stub");
 }
+
+
+//А вот это раскоментиваем
+// "use server";
+// import MainContent from "@/components/MainContent/MainContent";
+// import { auth } from "@/config/auth";
+
+// export default async function Home() {
+//   const session = await auth();
+
+//   const { token } = false;
+//   return (
+//     <div>
+//       {session && (
+//         <>
+//           <h1 style={{ textAlign: "center" }}>
+//             Добро пожаловать, {session.user?.name}
+//           </h1>
+//           <MainContent />
+//         </>
+//       )}
+//       {!session && (
+//         <h1 style={{ textAlign: "center", padding: 20 }}>
+//           Пожалуйста авторизируйтесь
+//         </h1>
+//       )}
+//     </div>
+//   );
+// }
